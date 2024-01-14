2d481f95cf61e1ed14a0a661247f8910b8370a8e
Sarper Makas <sarper.makas@superonline.com>
2024-01-14 20:23:50 +0300

    create directories according to extensions

--- main.py
+++ main.py
@@ -14,13 +14,25 @@ def get_desktop_path() -> str:
         case _: # Unix or Linux
             return  os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
         
-def get_file_type(files: list[str]) -> list[str]:
+def get_file_extensions(files: list[str]) -> list[str]:
+    """Get file extension as list without duplication"""
+    
     types = map(lambda x: x.split('.')[1], files)
     types_deduplicate = set(types)
     return list(types_deduplicate)
 
+def create_directories(file_extensions: list[str]) -> None:
+    """Create directories in the desktop according to specified extension names"""
+    
+    for file_extension in file_extensions:
+        path = os.path.join(desktop_path, file_extension.upper())
+        
+        # check whether the path is exist or not
+        if not os.path.exists(path):
+            os.mkdir(path)
+
 desktop_path = get_desktop_path()        
 onlyfiles = [f for f in os.listdir(desktop_path) if os.path.isfile(os.path.join(desktop_path, f))]
-types = get_file_type(onlyfiles)
+file_extensions = get_file_extensions(onlyfiles)
 
-print(types)
+create_directories(file_extensions)
