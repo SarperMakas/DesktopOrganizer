e316cf3892525dcb9039f96601589bc84953ec19
Sarper Makas <sarper.makas@superonline.com>
2024-01-14 20:08:48 +0300

    Getting files from desktop

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ def get_desktop_path() -> str:
             return os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
         case _: # Unix or Linux
             return  os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-        
+
+desktop_path = get_desktop_path()        
+onlyfiles = [f for f in os.listdir(desktop_path) if os.path.isfile(os.path.join(desktop_path, f))]
+
+
+print(desktop_path)
+print(onlyfiles)        
